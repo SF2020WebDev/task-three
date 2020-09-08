2b23370b65277638e087be7e34baa1fe85dd3248
Sam <sam.fordham1@gmail.com>
2020-09-08 17:03:07 +0100

    Nearly completed, just need to resolve the border issue

--- src/components/details.js
+++ src/components/details.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Details = (props) => {
+
+
+    const [bold, setBold] = useState(false)
     return(
-        <div>
-            <p> {props.name} {props.age}</p>
+        <div >
+            <p style={{borderStyle:"dashed"}}
+                style={
+                    bold ? {fontWeight:"bold"} : {fontWeight:"normal"} }>
+                        {props.name} </p>
+            <p style={{borderStyle:"dashed"}}>{props.age} </p>
+
+            <button onClick={()=> setBold(true)}>Make me bold!</button>
             
         </div>
     )
--- src/components/updateDetails.js
+++ src/components/updateDetails.js
@@ -3,48 +3,34 @@ import Details from './details.js'
 
 const UpdateDetails = () => {
     const [name, setName] = useState("")
-    useEffect(() => {
-        console.log(name)
-    }, [name]
-    )
 
     const [age, setAge] = useState("")
-    useEffect(() => {
-        console.log(age)
-    }, [age]
-    )
 
     const [click, setClick] = useState(0)
 
-    const Increase = () => {
+    const increase = () => {
         setClick(prevClick => prevClick + 1);
     
     }
+
     
-
-    const [complete, setComplete] = useState(false)
-
-    const [bold, setBold] = useState(false)
         
     
     return(
         <div>
-            <Details name={setName} 
-                     age={setAge} />
+            
             
             <input value={name} onChange={e => setName(e.target.value)} placeholder="name" />
             <input value={age} onChange={e => setAge(e.target.value)} placeholder="age" />
 
-            <button onClick={(setName, setAge, Increase, setComplete)}>Submit</button>
+            <button type="submit" onClick={()=>console.log(name,age)} onClick={(setName, setAge, increase)}>Submit</button>
             
-            {complete ? <p>Name: {name} </p> : null}
-            {complete ? <p>Age: {age}</p> : null}
+            
+            
+            <Details name={name} age={age}/>
+                     
+
             <p>Number of Clicks: {click}</p>
-
-            <button onClick={()=> setBold(true)}>Make me bold!</button>
-
-            
-            
 
 
         </div>
